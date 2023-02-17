b6f09178277fa656b92082b838485d21edadd34c
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-17 22:24:48 +0100

    <Route path="/exercise/:id" element=

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import "./App.css";
 function App() {
   return (
     <Box width="400px">
-      Navbar
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
     </Box>
   );

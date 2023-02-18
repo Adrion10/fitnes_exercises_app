d1107ec1bef9d8af1d99b9658421ce78f90ca45b
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-18 23:12:54 +0100

    import { BrowserRouter } from "react-router-dom";

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import "./App.css";
 import Home from "./pages/Home";
 import ExerciseDetail from "./pages/ExerciseDetail";
 import Navbar from "./pages/Navbar";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 }

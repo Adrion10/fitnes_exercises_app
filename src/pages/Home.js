891dbfc28f0385797682abbd0ad861e7844d4edf
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-20 22:44:04 +0100

    <HeroBaner />       <SearchExercises />       <Exercises />

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBaner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 };
 
 export default Home;

0cd942dbe96b487c62a0b7cdd8fe2fdb44da2818
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-20 22:59:31 +0100

    <Box       sx={{         mt: { lg: "212px", xs: "70px" },         ml: { sm: "50px" },       }}       position="relative"       p="20px"     >

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -3,7 +3,14 @@ import { Box, Stack, Typegraphy } from "@mui/material";
 
 const HeroBanner = () => {
   return (
-    <Box>
+    <Box
+      sx={{
+        mt: { lg: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
       <Typegraphy color="#FF2625" fontWeight="600" fontSize="26px"></Typegraphy>
     </Box>
   );

7b89d0c212dfac2bfbe195f60976bb462fd454ae
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-22 21:11:56 +0100

    onChange={(e) => setSearch(e.target. value.toLowerCase())}

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Stack, Box, Button, TextField, Typography } from "@mui/material";
+
 const SearchExercises = () => {
+  const [search, setSearch] = useState("");
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography
@@ -22,8 +24,8 @@ const SearchExercises = () => {
             borderRadius: "40px",
           }}
           height="76px"
-          value=""
-          onChange={(e) => {}}
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
           type="text"
         />
@@ -33,9 +35,10 @@ const SearchExercises = () => {
             bgcolor: "#ff2525",
             color: "#fff",
             textTransform: "none",
-            width: { lg: "60px", xs: "30px" },
+            width: { lg: "130px", xs: "80px" },
             height: "56px",
             position: "absolute",
+            right: "0",
           }}
         >
           Search

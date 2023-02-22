b0742dbdd6c497bd98482188fcdb33d7e0a97fb3
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-22 20:34:09 +0100

    <Stack alignItems="center" mt="37px"  justifyContent="center" p="20px">

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Stack, Box, Buttom, TextField, Typography } from "@mui/material";
 const SearchExercises = () => {
-  return <Stack alignItems="center" mt="37px"></Stack>;
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: "44px" },
+        }}
+      >
+        Awesome Exercise You <br /> Should now
+      </Typography>
+    </Stack>
+  );
 };
 
 export default SearchExercises;

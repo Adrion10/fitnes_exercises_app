637d123a211115b0c24b76f57d9b0e1f1e173ef1
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-24 21:09:43 +0100

    export const exerciseOptions

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,12 @@
+export const exerciseOptions = {
+  method: "GET",
+  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+  headers: {
+    "X-RapidAPI-Key": "ac15c909abmshc1f9e05404f21bap1c4186jsnc8db3c9e6c8a",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetchData(url, options);
   const data = await response.json();

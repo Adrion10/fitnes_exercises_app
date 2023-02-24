d37796e8dfa61415d4daf46247452745c8803b06
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-24 21:16:14 +0100

    process.env.RAPID_API_KEY,

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,7 @@
 export const exerciseOptions = {
   method: "GET",
-  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
   headers: {
-    "X-RapidAPI-Key": "ac15c909abmshc1f9e05404f21bap1c4186jsnc8db3c9e6c8a",
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };

a7d7649d85bf2b0441c1ff995288e7e595ed7216
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-24 21:21:52 +0100

    import

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Stack, Box, Button, TextField, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const SearchExercises = () => {
   const [search, setSearch] = useState("");
 
   const handleSearch = async () => {
     if (search) {
-      // const exerciseData = await fetchData();
+      const exerciseData = await fetchData();
     }
   };
   return (

c1e1e32d959cbce4487d5709c7b7b88cc1f8f8e5
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-25 21:42:02 +0100

    const SearchExercises = ({setExercises,bodyPart,setBodyPart})

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,7 +3,7 @@ import { Stack, Box, Button, TextField, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import HorizontalScrollbar from "../components/HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
   const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
@@ -79,7 +79,11 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
-        <HorizontalScrollbar data={bodyParts} />
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   );

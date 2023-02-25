5026da39dbdb567fdac0228e1582b22c5738270b
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-25 21:12:09 +0100

    <Box           key={item.id || item}

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const HorizontalScrollbar = () => {
-  return <div>HorizontalScrollbar</div>;
+const HorizontalScrollbar = ({ data }) => {
+  return (
+    <div>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="0 40px"
+        ></Box>
+      ))}
+    </div>
+  );
 };
 
 export default HorizontalScrollbar;

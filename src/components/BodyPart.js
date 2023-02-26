8b57ecc296d69093b5b683f3bf6bdab38c160068
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-26 21:18:39 +0100

    mport icon

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icon/gym.png";
 
 const BodyPart = () => {
-  return <div>BodyPart</div>;
+  return <Stack></Stack>;
 };
 
 export default BodyPart;

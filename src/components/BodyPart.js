2fed8f06c0413024dba8dadabb97a3fd40e2b12e
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-26 21:23:37 +0100

    <Stack>       <img src={Icon}

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,7 +3,11 @@ import { Stack, Typography } from "@mui/material";
 import Icon from "../assets/icon/gym.png";
 
 const BodyPart = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack>
+      <img src={Icon} alt="dumbell" style={{ width: "40px", height: "40px" }} />
+    </Stack>
+  );
 };
 
 export default BodyPart;

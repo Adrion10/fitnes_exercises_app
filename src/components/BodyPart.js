efbae2078e1f839d682cca6a93c6d01f941b38d1
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-26 21:27:50 +0100

    const BodyPart = ({ item, setBodyPart, bodyPart

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -2,9 +2,15 @@ import React from "react";
 import { Stack, Typography } from "@mui/material";
 import Icon from "../assets/icon/gym.png";
 
-const BodyPart = () => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <Stack>
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{}}
+    >
       <img src={Icon} alt="dumbell" style={{ width: "40px", height: "40px" }} />
     </Stack>
   );

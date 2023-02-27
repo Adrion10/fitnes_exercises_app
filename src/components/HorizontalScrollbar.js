943f4ed490c5d14b6ad3062f56f4ba65423f9509
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-27 21:56:47 +0100

    fixed

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
 import { Box, Typography } from "@mui/material";
+import ExerciseCard from "./ExerciseCard";
 import BodyPart from "./BodyPart";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
-
+import RightArrowIcon from "../assets/icons/right-arrow.png";
 import LeftArrowIcon from "../assets/icons/left-arrow.png";
 
 const LeftArrow = () => {

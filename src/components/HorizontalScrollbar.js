6a373f6674f4db6207519c10baf29b59869b6b56
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-27 21:51:45 +0100

    <Typography onClick={() => scrollPrev()

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -3,10 +3,16 @@ import { Box } from "@mui/material";
 import BodyPart from "./BodyPart";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 
-import LeftArrowIcon from '../assets/icons/left-arrow.png';
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (

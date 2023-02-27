dbf0614119e5a8015f3e3267e25b162fae75a211
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-02-27 22:51:16 +0100

    Exercises

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,7 +1,4 @@
-import React, { useEffect, useState } from "react";
-import Pagination from "@mui/material/Pagination";
-import { Box, Stack, Typography } from "@mui/material";
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import React from "react";
 
 const ExerciseCard = () => {
   return <div>ExerciseCard</div>;
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
-const Exercises = () => {
-  return <div>Exercises</div>;
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  return (
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography varianat="h4">Showing Results</Typography>
+    </Box>
+  );
 };
 
 export default Exercises;

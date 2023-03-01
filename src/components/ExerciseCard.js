6cd2e28fe1eda8a7081e28efdccd7d3a2ae6a681
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-01 22:18:17 +0100

    Link

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Stack, Typography } from "@mui/material";
 
-const ExerciseCard = () => {
-  return <div>ExerciseCard</div>;
+const ExerciseCard = ({ exercise }) => {
+  return (
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}></Link>
+  );
 };
 
 export default ExerciseCard;

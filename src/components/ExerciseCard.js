b02c9880845fcf163adcb3d741f9a74a75823fb3
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-01 22:35:06 +0100

    Button

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -6,6 +6,20 @@ const ExerciseCard = ({ exercise }) => {
   return (
     <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
       <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "#fff",
+            background: "#ffa9a9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+      </Stack>
     </Link>
   );
 };

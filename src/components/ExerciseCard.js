117ac4ef4ad18ad0f1db039c07343250c343fa2c
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-01 22:22:45 +0100

    img

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -4,7 +4,9 @@ import { Button, Stack, Typography } from "@mui/material";
 
 const ExerciseCard = ({ exercise }) => {
   return (
-    <Link className="exercise-card" to={`/exercise/${exercise.id}`}></Link>
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+    </Link>
   );
 };
 

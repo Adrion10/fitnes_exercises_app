153fcb92ba7af735fea6df2080a6de1638b6c296
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-01 22:07:20 +0100

    <ExerciseCard />

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
@@ -14,7 +15,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         justifyContent="center"
       >
         {exercises.map((exercise, index) => (
-          <p>{exercise.name}</p>
+          <ExerciseCard />
         ))}
       </Stack>
     </Box>

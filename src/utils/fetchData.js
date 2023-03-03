4be3588204ef03ea8e346268e1300015ff3c04f2
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-03 21:33:01 +0100

    export const fetchData

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,12 +2,13 @@ export const exerciseOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
   },
 };
 
 export const fetchData = async (url, options) => {
-  const response = await fetchData(url, options);
-  const data = await response.json();
+  const res = await fetch(url, options);
+  const data = await res.json();
+
   return data;
 };

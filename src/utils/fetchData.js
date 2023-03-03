db09c00fb15f8b7e2ff9711274323916d11ebdc3
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-03 21:31:18 +0100

    export const exerciseOptions

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,8 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 

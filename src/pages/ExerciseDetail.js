a72166db5c97675cbab9a9d93e15f46bb569aaaf
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-07 21:11:43 +0100

    import

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const ExerciseDetail = () => {
   return <div>ExerciseDetail</div>;

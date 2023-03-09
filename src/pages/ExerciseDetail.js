b1483f359cdef5723feb6ffbe6fc97621b54cf38
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-09 20:15:14 +0100

    import ExerciseVideo

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -4,6 +4,7 @@ import { Box } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import Detail from "../components/Detail";
+import ExerciseVideo from "../components/ExerciseVideo";
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});

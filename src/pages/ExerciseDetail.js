c35ac71abece02d70e5f9a6b9a81e3b760680208
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-09 20:14:18 +0100

    import Detail

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import Detail from "../components/Detail";
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});

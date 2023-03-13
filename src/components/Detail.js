7a0854956df42033513c0fbaa2b3ce388cd99705
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-13 17:16:29 +0100

    TargetImage

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Typography, Stack, ButtonClassKey } from "@mui/material";
 import BadyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
 
 const Detail = () => {
   return <div>Detail</div>;

14c00d0a8fd74268dc259c63a5a3ff739684f8f9
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-03-13 17:14:54 +0100

    BadyPartImage

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Typography, Stack, ButtonClassKey } from "@mui/material";
+import BadyPartImage from "../assets/icons/body-part.png";
 
 const Detail = () => {
   return <div>Detail</div>;
